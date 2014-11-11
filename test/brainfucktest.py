c8e4840bed28797935fcb29fb5c0bc3720951ef8
stribika <stribika@gmail.com>
2014-11-11 01:06:53 +0100

    More and better tests.

--- test/brainfucktest.py
+++ test/brainfucktest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import collections
+import profile
 import random
 import sys
 
@@ -8,6 +9,11 @@ sys.path.append('../main')
 
 from brainfuck import *
 from prng import AESCTR
+
+def run_test(test):
+    print('Running', test)
+    profile.run(test, sort = 2)
+    print('Finished', test)
 
 def generator():
     bfg = BFG(AESCTR(b'\x00' * 32, b'\x00' * 16))
@@ -24,31 +30,49 @@ def generator():
                 d -= 1
             assert d >= 0, 'mismatched ]'
         assert d == 0, 'mismatched ['
-generator()
+run_test('generator()')
 
-def cat_normal():
-    bfvm = BFVM('+[>,.<]', 2)
-    input = iter(range(1000))
+def cat_normal(bf_impl):
+    bfvm = bf_impl('+[>,.<]', 2)
+    input = iter(range(200))
 
-    output = bfvm.eval(input, 2 + 5 * 1000)
+    output = bfvm.eval(input, 2 + 5 * 200)
 
-    assert list(range(1000)) == output, 'cat failed'
-cat_normal()
+    assert list(range(200)) == output, 'cat failed'
+run_test('cat_normal(BFVM)')
+run_test('cat_normal(BFJIT)')
 
-def cat_pc_wrap():
-    bfvm = BFVM(',.', 1)
-    input = iter(range(1000))
+def cat_pc_wrap(bf_impl):
+    bfvm = bf_impl(',.', 1)
+    input = iter(range(200))
 
-    output = bfvm.eval(input, 2 * 1000)
+    output = bfvm.eval(input, 2 * 200)
 
-    assert list(range(1000)) == output, 'cat with PC wrapping failed'
-cat_pc_wrap()
+    assert list(range(200)) == output, 'cat with PC wrapping failed'
+run_test('cat_pc_wrap(BFVM)')
+run_test('cat_pc_wrap(BFJIT)')
 
-def cat_ptr_wrap():
-    bfvm = BFVM('+[<,.>]', 2)
-    input = iter(range(1000))
+def cat_ptr_wrap(bf_impl):
+    bfvm = bf_impl('+[<,.>]', 2)
+    input = iter(range(200))
 
-    output = bfvm.eval(input, 2 + 5 * 1000)
+    output = bfvm.eval(input, 2 + 5 * 200)
 
-    assert list(range(1000)) == output, 'cat with PTR wrapping failed'
-cat_ptr_wrap()
+    assert list(range(200)) == output, 'cat with PTR wrapping failed'
+run_test('cat_ptr_wrap(BFVM)')
+run_test('cat_ptr_wrap(BFJIT)')
+
+def random_compare():
+    bfg    = BFG(AESCTR(b'\xff' * 32, b'\x00' * 16))
+    in_int = AESCTR(b'\x00' * 32, b'\xff' * 16)
+    in_jit = AESCTR(b'\x00' * 32, b'\xff' * 16)
+
+    for i in range(10):
+        bf = bfg.random_bf(1024)
+        bfint = BFVM(bf, 65536)
+        bfjit = BFJIT(bf, 65536)
+        out_int = bfint.eval(in_int.bytes(), 1000000)
+        out_jit = bfjit.eval(in_jit.bytes(), 1000000)
+        assert out_int == out_jit, 'interpreted and compiled version different'
+run_test('random_compare()')
+
